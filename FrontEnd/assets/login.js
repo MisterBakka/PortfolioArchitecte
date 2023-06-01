a597fa4fb1962299ad86fc823a630acb647a6ce5
badre dahmouni <badredahmouni@gmail.com>
2023-06-01 17:57:19 +0200

    git apres suppr image

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,37 @@
+// Récupérons le formulaire HTML
+const formulaire = document.getElementById("login");
+
+// Définissons une fonction qui sera appelée lors de la soumission du formulaire
+async function soumettreFormulaire(event) {
+  event.preventDefault(); // Empêche le formulaire de se soumettre
+
+  const email = formulaire.email.value;
+  const password = formulaire.password.value;
+
+  const dataForm = {
+    email: email,
+    password: password
+  };
+
+  const response = await fetch(`http://localhost:5678/api/users/login`, {
+    method: "POST",
+    body: JSON.stringify(dataForm),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  if (!response.ok) {
+    const errorMessage = "Erreur dans l'identifiant ou le mot de passe";
+    const messageErreur = document.getElementById("wrong-psw");
+    messageErreur.textContent = errorMessage;
+    return;
+  }
+
+  const data = await response.json();
+  localStorage.setItem("token", data.token);
+  window.location.href = "index.html";
+}
+
+// Ajoutons un EvenListener sur le formulaire
+formulaire.addEventListener("submit", soumettreFormulaire);
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,95 +1,324 @@
-// Récupération des éléments DOM
-const gallery = document.getElementById('gallery'); // Élément DOM de la galerie d'images
-const filterContainer = document.getElementById('filter-container'); // Élément DOM du conteneur de filtrage
+let tokenValue = localStorage.token;
+const filterContainer = document.getElementById('filter-container');
+const gallery = document.querySelector('.gallery');
+const galleryEdit = document.querySelector('.galleryEdit');
+const change = document.getElementById('change');
 
-// Appel à l'API pour obtenir les données des œuvres
-fetch('http://localhost:5678/api/works')
-    .then(response => response.json()) // Convertit la réponse en format JSON
-    .then(data => {
-        parcourirTableau(data); // Appelle la fonction pour afficher les données des œuvres
-})
+const fetchWorks = async () => {
+    const response = await fetch('http://localhost:5678/api/works');
+    const data = await response.json();
 
-// Fonction pour parcourir les données des œuvres et les afficher
-function parcourirTableau(data) {
-    data.forEach(works => {
+    // Parcourir les données et créer un élément image pour chaque image
+    if (data) {
+        data.forEach(work => {
+            const workElement = document.createElement('figure');
+            const imageElement = document.createElement('img');
+            imageElement.setAttribute('src', work.imageUrl);
+            imageElement.setAttribute('alt', work.title);
+            workElement.appendChild(imageElement);
 
-        const figureElement = document.createElement('figure'); // Crée un élément <figure>
+            const titleElement = document.createElement('figcaption');
+            titleElement.textContent = work.title;
+            workElement.appendChild(titleElement);
 
-        const imgElement = document.createElement('img'); // Crée un élément <img>
-        imgElement.src = works.imageUrl; // Définit l'URL de l'image
-        imgElement.alt = works.title; // Définit l'attribut alt de l'image
+            gallery.appendChild(workElement);
+        });
+    } else {
+        console.log(data);
+    }
+};
 
-        const figcaptionElement = document.createElement('figcaption'); // Crée un élément <figcaption>
-        figcaptionElement.textContent = works.title; // Définit le texte du <figcaption>
+const fetchWorksEdit = async () => {
+    const response = await fetch('http://localhost:5678/api/works');
+    const data = await response.json();
 
-        figureElement.appendChild(imgElement); // Ajoute l'élément <img> à l'élément <figure>
-        figureElement.appendChild(figcaptionElement); // Ajoute l'élément <figcaption> à l'élément <figure>
+    // Parcourir les données et créer un élément image pour chaque image
+    if (data) {
+        data.forEach((work, index) => {
+            const workElement = document.createElement('div');
+            workElement.classList.add("imgEdit");
 
-        figureElement.dataset.categoryId = works.categoryId; // Associe l'ID de catégorie à l'attribut data-categoryId de l'élément <figure>
+            const imageElement = document.createElement('img');
+            imageElement.setAttribute('src', work.imageUrl);
+            imageElement.setAttribute('alt', work.title);
+            workElement.appendChild(imageElement);
+            //Arrow position 
+            if (index === 0) {
+                const iconArrowElement = document.createElement('i');
+                iconArrowElement.classList.add('fa-solid', 'fa-arrows-up-down-left-right', 'absolute');
+                workElement.appendChild(iconArrowElement);
+            }
 
-        gallery.appendChild(figureElement); // Ajoute l'élément <figure> à la galerie
-    });
-}
+            const iconTrashElement = document.createElement('i');
+            iconTrashElement.classList.add('fa-solid', 'fa-trash-can', 'absolute', 'delete');
+            iconTrashElement.id = work.id;
+            workElement.appendChild(iconTrashElement);
+            iconTrashElement.addEventListener('click', () => deleteWork(work.id)
+            );
 
-// Appel à l'API pour obtenir les données des catégories
-fetch('http://localhost:5678/api/categories')
-    .then(response => response.json()) // Convertit la réponse en format JSON
-    .then(data => {
-        console.log(data); // Affiche les données des catégories dans la console
-        FilterOptions(data); // Appelle la fonction pour créer les options de filtrage
-});
+            const titleElement = document.createElement('p');
+            titleElement.textContent = 'éditer';
+            workElement.appendChild(titleElement);
 
-// Fonction pour créer les options de filtrage
-function FilterOptions(categories) {
-    const optionTous = document.createElement('button'); // Crée un bouton pour l'option "Tous"
-    optionTous.value = 'tous'; // Définit la valeur du bouton
-    optionTous.textContent = 'Tous'; // Définit le texte du bouton
-    filterContainer.appendChild(optionTous); // Ajoute le bouton au conteneur de filtrage
-    console.log(optionTous);
+            galleryEdit.appendChild(workElement);
+        });
+    } else {
+        console.log(data);
+    }
+};
 
-    optionTous.addEventListener('click', function () {
-        allImages(optionTous.value); // Appelle la fonction pour afficher toutes les images
-    });
 
-    categories.forEach(category => {
-        const optionElement = document.createElement('button'); // Crée un bouton pour chaque catégorie
-        optionElement.value = category.id; // Définit la valeur du bouton en utilisant l'ID de la catégorie
-        optionElement.textContent = category.name; // Définit le texte du bouton en utilisant le nom de la catégorie
-        filterContainer.appendChild(optionElement); // Ajoute le bouton au conteneur de filtrage
-        console.log(optionElement);
 
-        optionElement.addEventListener('click', function () {
-            filterImagesCategory(optionElement.value); // Appelle la fonction pour filtrer les images par catégorie
+const fetchCategories = async () => {
+    const response = await fetch('http://localhost:5678/api/categories');
+    const categories = await response.json();
+    // Bouton "Tous"
+    const allButton = document.createElement('button');
+    allButton.textContent = 'Tous';
+    allButton.addEventListener('click', async () => {
+        const response = await fetch('http://localhost:5678/api/works');
+        const works = await response.json();
+        gallery.innerHTML = '';
+
+        works.forEach(work => {
+            const workElement = document.createElement('figure');
+
+            const imageElement = document.createElement('img');
+            imageElement.src = work.imageUrl;
+            imageElement.alt = work.title;
+            workElement.appendChild(imageElement);
+
+            const titleElement = document.createElement('figcaption');
+            titleElement.textContent = work.title;
+            workElement.appendChild(titleElement);
+
+            gallery.appendChild(workElement);
         });
     });
-}
 
-// Fonction pour afficher toutes les images ou filtrer par catégorie
-function allImages(categoryId) {
-    const allImages = Array.from(document.querySelectorAll('#gallery figure')); // Récupère toutes les images de la galerie
-    allImages.forEach(image => {
-        if (categoryId === 'tous') {
-            image.style.display = 'block'; // Affiche l'image si la catégorie sélectionnée est "tous"
-        } else {
-            const imageCategory = image.dataset.categoryId;
-            if (imageCategory === categoryId) {
-                image.style.display = 'block'; // Affiche l'image si elle appartient à la catégorie sélectionnée
-            } else {
-                image.style.display = 'none'; // Masque l'image si elle n'appartient pas à la catégorie sélectionnée
-            }
+    filterContainer.appendChild(allButton);
+
+    categories.forEach(category => {
+        const filterElement = document.createElement('button');
+        filterElement.textContent = category.name;
+
+        filterElement.addEventListener('click', async () => {
+            const response = await fetch(`http://localhost:5678/api/works?categoryId=${category.id}`);
+            const works = await response.json();
+            gallery.innerHTML = '';
+            const workElement = document.createElement('figure');
+            works.forEach(work => {
+                if (work.categoryId === category.id) {
+                    const workElement = document.createElement('figure');
+
+                    const imageElement = document.createElement('img');
+                    imageElement.src = work.imageUrl;
+                    imageElement.alt = work.title;
+                    workElement.appendChild(imageElement);
+
+                    const titleElement = document.createElement('figcaption');
+                    titleElement.textContent = work.title;
+                    workElement.appendChild(titleElement);
+
+                    gallery.appendChild(workElement);
+                }
+            });
+        });
+        filterContainer.appendChild(filterElement);
+    });
+};
+
+
+
+
+const deleteWork = async (id) => {
+    //const deleteIcons = document.querySelectorAll('.fa-trash-can');
+    console.log(id);
+    // deleteIcons.forEach(icon => {
+    //         const workElement = event.target.closest('.imgEdit');
+    //         console.log(icon.id);
+    //         console.log('ok');
+    //         const imageElement = workElement.querySelector('img');
+    //         const imageSrc = imageElement.getAttribute('src');
+    //         const workId = workElement.dataset.workId;
+
+    const response = await fetch(`http://localhost:5678/api/works/${id}`, {
+        method: 'DELETE',
+        headers: {
+            //accept: "application/json",
+            'Content-Type': "application/json",
+            'Authorization': `Bearer ${tokenValue}`
         }
     });
-}
+    if (response.ok) {
+        //workElement.remove();
+        console.log('Supprimé');
+    } else {
+        console.error(`HTTP error! Status: ${response.status}`);
+    }
+};
 
-// Fonction pour filtrer les images par catégorie
-function filterImagesCategory(categoryId) {
-    const allImages = Array.from(document.querySelectorAll('#gallery figure')); // Récupère toutes les images de la galerie
-    allImages.forEach(image => {
-        const imageCategory = image.dataset.categoryId;
-        if (imageCategory === categoryId || categoryId === 'tous') {
-            image.style.display = 'block'; // Affiche l'image si elle appartient à la catégorie sélectionnée ou si la catégorie sélectionnée est "tous"
+function adminMode() {
+    const changeBar = document.getElementById('changeBar');
+    if (localStorage.getItem('token')) {
+        filterContainer.style.display = "none";
+        gallery.style.marginTop = "4rem";
+        change.style.display = "flex";
+        changeBar.style.display = "flex";
+
+    } else {
+        // si l'élément avec l'ID "container-filter" n'existe pas dans le document HTML
+    }
+    console.log(filterContainer);
+};
+
+const loginElement = document.getElementById('logout');
+
+if (localStorage.getItem('token')) {
+    loginElement.textContent = 'logout';
+} else {
+    loginElement.textContent = 'login';
+};
+
+loginElement.addEventListener('click', () => {
+    if (localStorage.getItem('token')) {
+        localStorage.removeItem('token');
+        loginElement.textContent = 'Login';
+    }
+});
+
+
+const modalEdit = document.getElementById('modalEdit');
+const openerEdit = document.getElementById('change');
+let closeEdit = document.querySelector('#modalEdit .close');
+const buttonEdit = document.getElementById('buttonEdit');
+const modalFile = document.getElementById('modalFile');
+let closeFile = document.querySelector('#modalFile .close');
+let currentModal = null; // stocke la référence au modal actuellement ouvert
+
+// Ajouter un écouteur d'événement au document
+document.addEventListener('click', function (e) {
+    // Vérifier si l'élément cliqué est le bouton d'ouverture du modal "modalEdit"
+    if (e.target === openerEdit) {
+        if (currentModal !== modalEdit) { // si un autre modal est ouvert
+            if (currentModal) currentModal.style.display = 'none'; // fermer le modal actuel
+            modalEdit.style.display = 'block'; // ouvrir le modal "modalEdit"
+            currentModal = modalEdit; // mettre à jour la référence au modal actuel
+        }
+    }
+    // Vérifier si l'élément cliqué est le bouton d'ouverture du modal "modalFile"
+    else if (e.target === buttonEdit) {
+        if (currentModal !== modalFile) { // si un autre modal est ouvert
+            if (currentModal) currentModal.style.display = 'none'; // fermer le modal actuel
+            modalFile.style.display = 'block'; // ouvrir le modal "modalFile"
+            currentModal = modalFile; // mettre à jour la référence au modal actuel
+        }
+    }
+    // Fermer le modal actuel si on clique en dehors de celui-ci ou sur le bouton close
+    else if (currentModal && ((!currentModal.contains(e.target) && e.target !== closeEdit) || (currentModal === modalFile && e.target === closeFile))) {
+        currentModal.style.display = 'none';
+        currentModal = null; // mettre à jour la référence au modal actuel
+    }
+    // Vérifier si l'élément cliqué est la classe "left"
+    else if (e.target.classList.contains('left')) {
+        if (currentModal === modalFile) { // si modalFile est actuellement ouvert
+            modalFile.style.display = 'none'; // fermer modalFile
+            modalEdit.style.display = 'block'; // ouvrir modalEdit
+            currentModal = modalEdit; // mettre à jour la référence au modal actuel
+        }
+    }
+});
+
+// Ajouter un écouteur d'événement sur le bouton close de modalEdit
+closeEdit.addEventListener('click', function () {
+    if (currentModal === modalEdit) {
+        currentModal.style.display = 'none';
+        currentModal = null;
+    }
+});
+
+
+
+
+
+//test
+function fetchAddWork() {
+    const titreInput = document.getElementById('titre');
+    const categorieInput = document.getElementById('choix');
+    const sendInput = document.getElementById('send');
+    const addFile = document.getElementById('file');
+    const previewDiv = document.getElementById('preview');
+    const formForget = document.getElementById('form-forget');
+
+    sendInput.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const formData = new FormData();
+        formData.append('title', titreInput.value);
+        formData.append('category', categorieInput.value);
+        formData.append('image', addFile.files[0]);
+
+        if (titreInput.value && categorieInput.value && addFile.files[0]) {
+            fetch('http://localhost:5678/api/works', {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    Authorization: `Bearer ${tokenValue}`,
+                },
+
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log('Ajout de travail réussi :', data);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
         } else {
-            image.style.display = 'none'; // Masque l'image si elle n'appartient pas à la catégorie sélectionnée
+            console.error('Tous les champs doivent être remplis')
+            formForget.style.display = 'block';
         }
     });
+};
+
+
+
+
+//voir l'image
+const fileInput = document.getElementById("file");
+const previewDiv = document.getElementById("preview");
+
+fileInput.addEventListener("change", function () {
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener("load", function () {
+            previewDiv.innerHTML = `<img src="${this.result}" alt="preview image">`;
+        });
+
+        reader.readAsDataURL(file);
+
+        // Hide the upload icon
+        const uploadIcon = document.querySelector("#fileImg i");
+        uploadIcon.style.display = "none";
+    }
+});
+
+
+// appel des fonctions
+
+function initScript() {
+    fetchWorks();
+    fetchWorksEdit();
+    fetchCategories();
+    adminMode();
+    deleteWork();
+    fetchAddWork();
 }
+initScript();
+
+
+
+
